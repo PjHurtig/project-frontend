960cc97311ab546d6223193d67bb5ecbbfbffb9b
PjHurtig <>
2023-09-06 13:22:50 +0000

    add set events for calendarpage

--- src/pages/calendars/CalendarPage.js
+++ src/pages/calendars/CalendarPage.js
@@ -1,14 +1,71 @@
-import React from 'react'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import React, { useEffect, useState } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
-export default class CalendarPage extends React.Component {
-  render() {
-    return (
-      <FullCalendar
-        plugins={[ dayGridPlugin ]}
-        initialView="dayGridMonth"
-      />
-    )
-  }
-}
+
+
+
+const EventCalendar = () => {
+  const [events, setEvents] = useState([]);
+  const history = useHistory();
+
+  const onEventClick = (eventClickInfo) => {
+    const eventId = eventClickInfo.event.id;
+
+    history.push(`/events/${eventId}`);
+  };
+
+  const getCategoryColor = (category) => {
+    switch (category) {
+      case 'bike':
+        return '#E9C46A';
+      case 'climbing':
+        return '#264653'; 
+      case 'hiking':
+        return '#E76F51'; 
+      default:
+        return '#606c38'; 
+    }
+  };
+  
+  
+  
+  useEffect(() => {
+    axios.get('/events/')
+      .then(response => {
+        console.log('response:', response.data.results);
+  
+        const eventsData = response.data.results.map(event => ({
+          id: event.id,
+          title: event.title,
+          start: new Date(event.start_time), 
+          backgroundColor: getCategoryColor(event.category),
+        }));
+  
+        setEvents(eventsData);
+      })
+      .catch(error => {
+        console.error('Error fetching events:', error);
+      });
+  }, []);
+
+  return (
+
+        <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridWeek"
+        events={events}
+        eventClick={onEventClick}
+        eventTimeFormat={{
+            hour: 'numeric',
+            minute: '2-digit',
+            hour12: false, 
+        }}
+        />
+
+  );
+};
+
+export default EventCalendar;
