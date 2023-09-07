8e91448020094a7d15b8614d8b891463f1bbddbe
PjHurtig <>
2023-09-07 15:48:34 +0000

    add style to gearitem

--- src/pages/gearitems/GearItem.js
+++ src/pages/gearitems/GearItem.js
@@ -5,7 +5,7 @@ import Avatar from "../../components/Avatar";
 import { MoreDropdown } from "../../components/MoreDropdown";
 import GearItemEditForm from "./GearItemEditForm";
 
-import styles from "../../styles/Comment.module.css";
+import styles from "../../styles/GearItem.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { axiosRes } from "../../api/axiosDefaults";
 
@@ -50,14 +50,22 @@ const GearItem = (props) => {
     <>
       <Accordion defaultActiveKey="1">
         <Card>
-          <Accordion.Toggle as={Card.Header} eventKey="0">
+          <Accordion.Toggle 
+            as={Card.Header} 
+            eventKey="0"
+            className={styles.Header}
+            >
           <i className="fa-solid fa-chevron-down"></i>
-          <span className={styles.Owner}>{name}</span>
+          <span className={styles.Name}>{name}</span>
           <span className={styles.Date}>{updated_at}</span>
           </Accordion.Toggle>
           <Accordion.Collapse eventKey="0">
-            <Card.Body>
-              <Card.Img variant="top-left" src={image} />
+            <Card.Body className={styles.Body}>
+              <Card.Img 
+              variant="top" 
+              src={image} 
+              className={styles.Img}
+              />
 
                 {showEditForm ? (
                   <GearItemEditForm
@@ -68,6 +76,7 @@ const GearItem = (props) => {
                     profileImage={profile_image}
                     setGearItems={setGearItems}
                     setShowEditForm={setShowEditForm}
+                    image={image}
                   />
                 ) : (
                   <Card.Text>
