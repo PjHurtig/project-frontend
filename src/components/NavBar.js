4b8ad5072ef70697f55ad03aa16eddbf52da6889
PjHurtig <>
2023-09-08 02:06:38 +0000

    prepare for deployment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,6 +7,7 @@ import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContex
 import styles from '../styles/NavBar.module.css'
 import useClickOutsideToggle from '../hooks/useClickOutsideToggle'
 import Avatar from './Avatar'
+import { removeTokenTimestamp } from '../utils/utils'
 
 
 const NavBar = () => {
@@ -19,6 +20,7 @@ const NavBar = () => {
         try {
           await axios.post("dj-rest-auth/logout/");
           setCurrentUser(null);
+          removeTokenTimestamp();
         } catch (err) {
           console.log(err);
         }
