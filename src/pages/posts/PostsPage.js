c21a56fe32240af87d00d117d18af6db81e8dfe1
PjHurtig <>
2023-09-08 01:25:48 +0000

    fix layout of postspage

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -16,7 +16,7 @@ import { axiosReq } from "../../api/axiosDefaults";
 import NoResults from "../../assets/no-results.png";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
-import UpcomingEvents from "../events/UpcomingEvents";
+
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
@@ -49,7 +49,6 @@ function PostsPage({ message, filter = "" }) {
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2 mx-auto" lg={8}>
-        <p>Popular profiles mobile</p>
         <i className={`fas fa-search ${styles.SearchIcon}`} />
         <Form
           className={styles.SearchBar}
